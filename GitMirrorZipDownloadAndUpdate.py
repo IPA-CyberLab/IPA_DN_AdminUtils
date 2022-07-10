b86fce0e1a0b1efaff0156ee427d466b9983e0f8
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-07-10 15:15:21 +0900

    z

--- GitMirrorZipDownloadAndUpdate.py
+++ GitMirrorZipDownloadAndUpdate.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     remote_commit_filepath = zip_src_dir + "/commit_id.txt"
 
-    EasyExec.Run(["/usr/bin/curl", "--get", "--globoff", "--fail", "-k", "--pinnedpubkey", ssl_pubkey, git_mirror_url + "/_git_current_commit_id.txt", "-o", remote_commit_filepath])
+    EasyExec.Run(["curl", "--get", "--globoff", "--fail", "-k", "--pinnedpubkey", ssl_pubkey, git_mirror_url + "/_git_current_commit_id.txt", "-o", remote_commit_filepath], False)
 
     remote_commit_id = Str.GetFirstFilledLine(Lfs.ReadAllText(remote_commit_filepath))[0]
 
